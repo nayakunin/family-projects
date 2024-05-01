565aa07223f6e06ac044119b52cf3dc8c44b3983
nayakunin <nayakunin99@gmail.com>
2024-05-01 10:16:02 +0200

    feat: 🎸 add button component

--- apps/poker/app/page.tsx
+++ apps/poker/app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from '@/components/ui/button';
+
 export default function Page() {
-    return <h1 className="text-3xl font-bold underline">Page</h1>;
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline">Page</h1>
+            <Button>Click me</Button>
+        </>
+    );
 }
