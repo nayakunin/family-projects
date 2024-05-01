8bd43ad038392c08bef438e5188b51ad3d44f80b
Nikita Yakunin <54310264+nayakunin@users.noreply.github.com>
2024-05-01 16:47:40 +0200

    Revert "Revert "Poker setup""

---
+++ apps/poker/.eslintrc.js
@@ -0,0 +1,14 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+    root: true,
+    extends: ['@repo/eslint-config/next.js'],
+    overrides: [
+        {
+            files: ['*.ts', '*.tsx'],
+            parser: '@typescript-eslint/parser',
+            parserOptions: {
+                project: true,
+            },
+        },
+    ],
+};
