f9046a12320ce5008d2b8e3164b7b8a4eba0cb14
nayakunin <nayakunin99@gmail.com>
2024-05-02 16:34:50 +0200

    feat: 🎸 clean up button

--- apps/poker/app/page.tsx
+++ apps/poker/app/page.tsx
@@ -1,31 +1,10 @@
-'use client';
-
 import { Button } from '@/components/ui/button';
-import { createRoom } from '@/server/actions';
 
 export default function Page() {
     return (
         <>
             <h1 className="text-3xl font-bold underline">Page</h1>
-            <Button
-                onClick={async () => {
-                    await createRoom({
-                        config: {
-                            gameType: 'online',
-                            startingChips: 1000,
-                            blindDurationUnit: 'hands',
-                            blindStructure: [
-                                {
-                                    bigBlind: 10,
-                                    smallBlind: 5,
-                                },
-                            ],
-                        },
-                    });
-                }}
-            >
-                Click me
-            </Button>
+            <Button>Click me</Button>
         </>
     );
 }
